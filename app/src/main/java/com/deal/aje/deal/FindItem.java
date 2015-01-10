a712c06f5ecbb99da33348b1dcf61f91d5b82619
Anthony Ang <anthonyang86@gmail.com>
2015-01-10 23:41:06 +0800

    FindItem UI Implemented, Current location acquired,

--- app/src/main/java/com/deal/aje/deal/FindItem.java
+++ app/src/main/java/com/deal/aje/deal/FindItem.java
@@ -1,17 +1,87 @@
 package com.deal.aje.deal;
 
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
-public class FindItem extends ActionBarActivity {
+public class FindItem extends ActionBarActivity implements LocationListener {
+    // Acquire a reference to the system Location Manager
+    LocationManager locationManager;
+    boolean locEnabled;
+    private String provider;
+    int curLat, curLong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_item);
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!locEnabled) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+        // Initialize the location fields
+        if (location != null) {
+            System.out.println("Provider " + provider + " has been selected.");
+            Toast.makeText(this, "Location Longitude: "+location.getLongitude()+"Location Latitude: "+location.getLatitude(),
+                    Toast.LENGTH_LONG).show();
+            onLocationChanged(location);
+        } else {
+            Log.d("DEAL_LOG", "Location Unavailable");
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        curLat = (int) (location.getLatitude());
+        curLong = (int) (location.getLongitude());
+    }
+
+    /* Request updates at startup */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+    /* Remove the locationlistener updates when Activity is paused */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
     }
 
 
