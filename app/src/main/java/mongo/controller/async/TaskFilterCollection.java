87c4e760babde36592f497339bac6f3be32c108d
gunarto90 <gunarto.nctu@gmail.com>
2015-01-11 23:24:24 +0800

    Mongo DB modified Filter Collection and Find All --> Using List<DBObject> Add registration id when register first time Add GcmController (Not fully tested)

--- app/src/main/java/mongo/controller/async/TaskFilterCollection.java
+++ app/src/main/java/mongo/controller/async/TaskFilterCollection.java
@@ -1,38 +1,56 @@
 package mongo.controller.async;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.Mongo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import mongo.controller.DbController;
 
 /**
  * Created by Lalala on 1/6/15.
  */
-public class TaskFilterCollection extends AsyncTask<Object, Void, DBCursor> {
+public class TaskFilterCollection extends AsyncTask<Object, Void, List<DBObject>> {
+    String TAG = "DEAL_" + TaskFilterCollection.class;
+
     @Override
-    protected DBCursor doInBackground(Object... params) {
-        if(params==null || params.length!=3) return null;
+    protected List<DBObject> doInBackground(Object... params) {
+        if (params == null || params.length < 1) return null;
         Mongo mongoClient = DbController.getInstance().Open();
-        if(mongoClient==null) return null;
+        if (mongoClient == null) return null;
         DB db = mongoClient.getDB(DbController.database);
         DBCollection coll = db.getCollection(params[0].toString());    // Table's name
-        String column = (String) params[1];
-        String value = (String) params[2];
-        DBCursor find = null;
-        if(column==null && value==null){
-            find = coll.find();
+        String column = null;
+        String value = null;
+        if (params.length == 3) {
+            column = (String) params[1];
+            value = (String) params[2];
         }
-        else
-        {
+        DBCursor find = null;
+        List<DBObject> result = new ArrayList();
+        if (column == null && value == null) {
+            find = coll.find();
+        } else {
             BasicDBObject query = new BasicDBObject(column, value);
             find = coll.find(query);
         }
+        if(find!=null)
+        {
+            while (find.hasNext()) {
+                DBObject next = find.next();
+                result.add(next);
+                Log.d(TAG, next.toString());
+            }
+        }
         DbController.getInstance().Close();
-        return find;
+        return result;
     }
 }
